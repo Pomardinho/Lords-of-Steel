c4a80b039f0d1493a4ebc11959369b137d2a5711
Jan Pomar <janpose04@gmail.com>
2023-04-27 12:35:58 +0200

    Nou personatge, Humà

---
+++ src/lordsofsteel/Huma.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package lordsofsteel;
+
+public class Huma extends Personatge {
+    public Huma(int FOR, int CON, int VEL, int INT, int SOR, Arma arma) {
+        super(FOR, CON, VEL, INT, SOR, arma);
+    }
+    
+    @Override
+    public void calculaEstadistiquesDerivades() {
+        super.calculaEstadistiquesDerivades();
+        PS = CON + FOR + INT;
+    }
+}
