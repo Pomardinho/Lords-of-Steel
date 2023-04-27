e9501ddd1bc450738c28473f2fe3c9a7e3eccc32
Jan Pomar <janpose04@gmail.com>
2023-04-27 12:36:30 +0200

    Nou personatge, Maia

---
+++ src/lordsofsteel/Maia.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package lordsofsteel;
+
+public class Maia extends Personatge {
+    public Maia(int FOR, int CON, int VEL, int INT, int SOR, Arma arma) {
+        super(FOR, CON, VEL, INT, SOR, arma);
+    }
+    
+    @Override
+    public void calculaEstadistiquesDerivades() {
+        super.calculaEstadistiquesDerivades();
+        PA = INT + SOR + arma.WVEL + VEL;
+    }
+}
