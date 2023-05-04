dda1bfbf3e3a44030baf3b50374d7362bb686b0c
Jan Pomar <janpose04@gmail.com>
2023-05-04 12:32:15 +0200

    Nova classe Dau

---
+++ src/lordsofsteel/Dau.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package lordsofsteel;
+
+public class Dau {
+    final int MAX = 25;
+    
+    public int llencar() {
+        return (int)(Math.random() * MAX) + 1;
+    }
+}
